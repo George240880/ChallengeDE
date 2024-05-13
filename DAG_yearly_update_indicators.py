3b6df7966d7e3273754d23b7e443572b512efc9f
George240880 <jorgeaga2408@hotmail.com>
2024-05-13 12:24:57 -0600

    Add files via upload

---
+++ DAG_yearly_update_indicators.py
@@ -0,0 +1,60 @@
+from airflow import DAG
+from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime, timedelta
+import pandas as pd
+import yfinance as yf
+
+# Define la función para calcular y actualizar los agregados anuales
+def actualizar_agregados_anuales():
+    # Consultar los datos diarios de la tabla principal
+    query = """
+    SELECT 
+        YEAR(date) AS year,
+        NAME AS name,
+        AVG(open) AS avg_open,
+        AVG(high) AS avg_high,
+        AVG(low) AS avg_low,
+        AVG(close) AS avg_close,
+        AVG(volume) AS avg_volume,
+        SUM(volume) AS tot_volume
+    FROM 
+        public.all_stocks
+    GROUP BY 
+        YEAR(date),
+        NAME
+    """
+    daily_data = snowflake_hook.get_pandas_df(query)
+
+    # Cargar los resultados en la tabla de agregados anuales
+    daily_data.to_sql("avg_indicators_per_year", con=snowflake_engine, schema="public", if_exists="replace", index=False)
+    print("Agregados anuales del S&P 500 actualizados exitosamente en Snowflake.")
+
+# Define los argumentos por defecto del DAG
+default_args = {
+    'owner': 'jaga2408',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    'start_date': datetime(2024, 5, 13),
+}
+
+# Define el DAG
+dag = DAG(
+    'actualizacion_agregados_anuales_sp500',
+    default_args=default_args,
+    description='Actualización anual de agregados del S&P 500 en Snowflake',
+    schedule_interval=timedelta(days=365),  # Ejecutar una vez al año
+)
+
+# Define el operador para calcular y actualizar los agregados anuales
+actualizar_agregados = PythonOperator(
+    task_id='actualizar_agregados_anuales',
+    python_callable=actualizar_agregados_anuales,
+    dag=dag,
+)
+
+# Define la secuencia de tareas del DAG
+actualizar_agregados_anuales
